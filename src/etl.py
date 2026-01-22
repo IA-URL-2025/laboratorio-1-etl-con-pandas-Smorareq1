96316fe85c0e24b85fd6afa43b61a2764eab8bf2
Smorareq1 <smorareq1@gmail.com>
2026-01-21 22:32:39 -0600

    ETL labroatorio 01 - Sebastian Morales

--- src/etl.py
+++ src/etl.py
@@ -5,8 +5,20 @@ def run_etl():
     Implementa el proceso ETL.
     No cambies el nombre de esta función.
     """
-    # TODO: implementar
-    pass
+    df = pd.read_csv('data/citas_clinica.csv')
+    
+    df['paciente'] = df['paciente'].str.title()
+    df['especialidad'] = df['especialidad'].str.upper()
+    
+    df['fecha_cita'] = pd.to_datetime(df['fecha_cita'], errors='coerce')
+    df = df[df['fecha_cita'].notna()]
+    
+    df = df[df['estado'] == 'CONFIRMADA']
+    df = df[df['costo'] > 0]
+    
+    df['telefono'] = df['telefono'].fillna('NO REGISTRA')
+    
+    df.to_csv('data/output.csv', index=False)
 
 
 if __name__ == "__main__":
